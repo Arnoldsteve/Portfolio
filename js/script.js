8a078c65513d2c12e81cf55fe0b46fe97f7d57dd
Arnoldsteve <stevearnold9e@gmail.com>
2023-06-28 21:18:00 +0300

    Edition Three

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+};
+
+
 // scroll section active link
 
 let sections = document.querySelectorAll('section');
@@ -13,8 +24,41 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href='+ id + ']').classList.add('active');
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
             });
         };
     });
+
+//  Sticky navbar
+
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky',  window.screenY > 100);
+
+// remove toggle icon and navbar when click navbar link (scroll)
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
+
+// Scroll Reveal
+ScrollReveal({
+    //  reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left' });
+// ScrollReveal().reveal('.home-content p , .about-content', { origin: 'right' });
+
+// Typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Engineer', 'Network Administrator', 'Graphic Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});

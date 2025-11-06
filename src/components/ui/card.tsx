62b68cb4a045a0f648c271f14786784f0203f3cf
Arnoldsteve <stevearnold9e@gmail.com>
2025-11-06 20:10:10 +0300

    update card border and shadow

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xs border py-6 shadow-xs",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-sm border py-6 shadow-sm",
         className
       )}
       {...props}

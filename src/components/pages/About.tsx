b2c997dd8e7f57a5aec48ae5883540173df60f6e
Arnoldsteve <stevearnold9e@gmail.com>
2025-11-07 10:09:11 +0300

    update hero section

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -3,11 +3,12 @@ import { Button } from "@/components/ui/button";
 
 export const About = () => {
   return (
-    <section id="about" className="container mx-auto px-4 py-12 sm:py-24 bg-accent/20">
+    <section
+      id="about"
+      className="container mx-auto  px-4 sm:px-12 py-4 sm:py-24"
+    >
       <div className="flex flex-col md:flex-row items-center justify-center gap-x-12 gap-y-10">
-        
-        {/* Left Column - Image */}
-         <div className="hidden md:flex md:w-1/3 justify-center">
+        <div className="hidden md:flex md:w-1/3 justify-center">
           <div className="w-72 h-72 lg:w-80 lg:h-80 relative">
             <Image
               src="/profile-pic2.png"
@@ -18,7 +19,6 @@ export const About = () => {
           </div>
         </div>
 
-        {/* Right Column - Text Content */}
         <div className="md:w-2/3 text-center md:text-left">
           <h2 className="text-4xl font-bold tracking-tight">
             About <span className="text-cyan-400">Me</span>
@@ -26,19 +26,24 @@ export const About = () => {
           <h3 className="text-2xl font-semibold mt-2">
             Full Stack Software Engineer!
           </h3>
-          <p className="mt-4 max-w-2xl text-muted-foreground">
-            Results-driven Full Stack Engineer with expertise in the Node.js and PHP ecosystems. 
-            Demonstrated success in building and deploying high-impact, scalable applications, 
-            including a booking engine that increased conversion rates by 30% and a secure 
-            multi-tenant invoice system that reduced vulnerabilities by 95%.
+          <p className="mt-4 max-w-2xl text-muted-foreground text-justify">
+            Results-driven Full Stack Engineer with expertise in the Node.js and
+            PHP ecosystems. Demonstrated success in building and deploying
+            high-impact, scalable applications, including a booking engine that
+            increased conversion rates by 30% and a secure multi-tenant invoice
+            system that reduced vulnerabilities by 95%.
           </p>
-           <div className="mt-6">
+          <div className="mt-6">
             <a href="#projects">
-              <Button size="lg" className="cursor-pointer bg-cyan-500 hover:bg-cyan-500 hover:shadow-cyan-400/40 transition-all text-blaxk">Read More</Button>
+              <Button
+                size="lg"
+                className="cursor-pointer bg-cyan-500 hover:bg-cyan-500 hover:shadow-cyan-400/40 transition-all text-blaxk"
+              >
+                Read More
+              </Button>
             </a>
           </div>
         </div>
-
       </div>
     </section>
   );
--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -6,24 +6,69 @@ import { Github, Gitlab, Linkedin } from "lucide-react";
 import { TypeAnimation } from "react-type-animation";
 import Link from "next/link";
 
+const socialLinks = [
+  {
+    href: "https://linkedin.com/in/steve-arnold-otieno",
+    icon: (
+      <Linkedin className="h-4 w-4 hover:text-cyan-400 transition-colors" />
+    ),
+  },
+  {
+    href: "https://github.com/Arnoldsteve",
+    icon: <Github className="h-4 w-4 hover:text-cyan-400 transition-colors" />,
+  },
+  {
+    href: "https://gitlab.com/Arnoldsteve",
+    icon: <Gitlab className="h-4 w-4 hover:text-cyan-400 transition-colors" />,
+  },
+  {
+    href: "https://wa.me/254796335895",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-4 w-4 hover:text-cyan-400 transition-colors"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z" />
+      </svg>
+    ),
+  },
+];
+
 export const Hero = () => {
   return (
-    <section className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-12 sm:py-24">
-      <div className="md:w-1/2 text-center md:text-left">
-        <p className="text-lg">Hello, it&apos;s me</p>
-        <h1 className="text-2xl font-bold tracking-tight lg:text-4xl my-2">
-          Steve Arnold
-        </h1>
-        <h2 className="text-xl lg:text-2xl font-semibold">
-          And I&apos;m a{" "}
-          <span className="text-black-700"> Software Engineer</span>
-        </h2>
-        <p className="mt-4 max-w-xl text-muted-foreground">
-          Results-driven engineer building scalable and high-impact web
-          applications in the JavaScript, PHP and Python ecosystems.
-        </p>
+    <section className="container mx-auto  items-center justify-between px-4 sm:px-12 py-4 sm:py-24">
+      <div className="w-full md:text-left justify-between">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 ">
+          <div className="p-0">
+            <p className="text-lg">Hello, it&apos;s me</p>
+            <h1 className="text-2xl font-bold tracking-tight lg:text-4xl my-2">
+              Steve Arnold
+            </h1>
+            <h2 className="text-xl lg:text-2xl font-semibold">
+              And I&apos;m a{" "}
+              <span className="text-black-700">Software Engineer</span>
+            </h2>
+            <p className="mt-4 max-w-xl text-muted-foreground text-justify">
+              Results-driven engineer building scalable and high-impact web
+              applications in the JavaScript, PHP and Python ecosystems.
+            </p>
+          </div>
 
-        <div className="mt-6 flex flex-row h-full justify-between">
+          <div className="flex justify-center md:justify-end">
+            <div className="w-40 h-40 lg:w-52 lg:h-52 relative">
+              <Image
+                src="/profile-pic.png"
+                alt="Steve Arnold Otieno"
+                fill
+                className="rounded-full object-cover shadow-xs"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 flex flex-row h-full justify-between gap-2">
           <div className="mt-6">
             <Link
               href="/SteveOtieno_SoftwareEngineer_Resume.pdf"
@@ -35,46 +80,15 @@ export const Hero = () => {
               </Button>
             </Link>
           </div>
-          <div className="mt-6 flex justify-center md:justify-start gap-x-4">
-            <Button variant={"outline"}>
-              <a
-                href="https://linkedin.com/in/steve-arnold-otieno"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Linkedin className="h-4 w-4 hover:text-cyan-400 transition-colors" />
-              </a>
-            </Button>
-            <Button variant={"outline"}>
-              <a
-                href="https://github.com/Arnoldsteve"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Github className="h-4 w-4 hover:text-cyan-400 transition-colors" />
-              </a>
-            </Button>
-            <Button variant={"outline"}>
-              <a
-                href="https://gitlab.com/Arnoldsteve"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Gitlab className="h-4 w-4 hover:text-cyan-400 transition-colors" />
-              </a>
-            </Button>
+          <div className="mt-6 flex justify-center md:justify-start gap-x-2">
+            {socialLinks.map((link, index) => (
+              <Button key={index} variant="outline">
+                <a href={link.href} target="_blank" rel="noopener noreferrer">
+                  {link.icon}
+                </a>
+              </Button>
+            ))}
           </div>
-        </div>
-      </div>
-
-      <div className="mt-10 md:mt-0 md:w-1/2 flex justify-center">
-        <div className="w-80 h-80 lg:w-96 lg:h-96 relative">
-          <Image
-            src="/profile-pic.png"
-            alt="Steve Arnold Otieno"
-            fill
-            className="rounded-full object-cover shadow-xs"
-          />
         </div>
       </div>
     </section>

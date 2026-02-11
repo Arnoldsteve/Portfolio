3f544206e35aa2fc3660a3ba777fcdcb1f5b5c3b
Arnoldsteve <stevearnold9e@gmail.com>
2026-02-11 14:41:32 +0300

    update types

--- src/types/services-details.ts
+++ src/types/services-details.ts
@@ -2,7 +2,7 @@ export interface ServiceDetail {
   serviceId: string;
   title: string;
   description: string;
-  iconName: "Server" | "ShieldCheck" | "Cloud" | "Activity" | "TrendingUp";
+  iconName: "Server" | "ShieldCheck" | "Zap" | "Monitor";
   overview: string;
   keyFeatures: string[];
   technologies: string[];
